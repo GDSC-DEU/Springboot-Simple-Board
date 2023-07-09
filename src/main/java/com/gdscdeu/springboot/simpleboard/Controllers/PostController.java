381fc41bd508724235d2acf7cb2515b52ebe8d57
YeJun, Jung <31740224+yejun614@users.noreply.github.com>
2023-07-10 00:43:19 +0900

    Feat: 게시글 조회 기능 추가 (#10)
    * Fix: convention 수정
    
    * Fix: convention 수정
    
    * Feat: 게시글 id 조회 기능 추가
    
    * Feat: title 그리고 content에 따라 게시글 검색 기능 추가

--- src/main/java/com/gdscdeu/springboot/simpleboard/Controllers/PostController.java
+++ src/main/java/com/gdscdeu/springboot/simpleboard/Controllers/PostController.java
@@ -1,12 +1,12 @@
 package com.gdscdeu.springboot.simpleboard.Controllers;
 
+import com.gdscdeu.springboot.simpleboard.DTOs.FindPostsDto;
 import com.gdscdeu.springboot.simpleboard.Entites.Posts;
 import com.gdscdeu.springboot.simpleboard.Services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -17,10 +17,24 @@ public class PostController {
     PostService postService;
 
     @GetMapping("")
-    List<Posts> readAllPost(){
+    List<Posts> readAllPost() {
         return postService.getAllPosts();
     }
 
+    @GetMapping("/{postID}")
+    Posts readPost(@PathVariable long postID, HttpServletResponse response) {
+        Posts post = postService.getPost(postID);
+
+        if (post == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return null;
+        }
+
+        return post;
+    }
+
+    @PostMapping("/find")
+    List<Posts> findPosts(@RequestBody FindPostsDto filter) {
+        return postService.findPosts(filter);
+    }
 }
-
-
