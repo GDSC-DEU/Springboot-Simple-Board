cdb5180e05a81ad27ca78c96bfd810abb9bafbfa
YeJun, Jung <yejun614@naver.com>
2023-07-13 23:33:43 +0900

    Feat: UserRepositoty 추가

---
+++ src/main/java/com/gdscdeu/springboot/simpleboard/Repositorys/UserRepository.java
@@ -0,0 +1,7 @@
+package com.gdscdeu.springboot.simpleboard.Repositorys;
+
+import com.gdscdeu.springboot.simpleboard.Entites.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+}
