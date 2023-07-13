f2c2c6b6d51ccdce5c6b0fb1f7b6f1db24e69e78
YeJun, Jung <yejun614@naver.com>
2023-07-13 23:55:30 +0900

    Feat: SHA512 암호화 기능 추가

---
+++ src/main/java/com/gdscdeu/springboot/simpleboard/utils/HashGenerator.java
@@ -0,0 +1,28 @@
+package com.gdscdeu.springboot.simpleboard.utils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashGenerator {
+
+    public static String SHA512(String input) {
+        // reference: https://www.geeksforgeeks.org/sha-512-hash-in-java/
+
+        try {
+            MessageDigest sha512 = MessageDigest.getInstance("SHA-512");
+            byte[] digest = sha512.digest(input.getBytes());
+            String hash = new BigInteger(1, digest).toString(16);
+
+            while (hash.length() < 32) {
+                hash = "0" + hash;
+            }
+
+            return hash;
+
+        } catch (NoSuchAlgorithmException error) {
+            throw new RuntimeException(error);
+        }
+    }
+
+}
